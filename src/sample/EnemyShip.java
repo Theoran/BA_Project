bdefdc510407f6fda7b163db7d8f31da4567d474
fernitzfelix <fernitz@webit.de>
2018-09-26 21:30:54 +0200

    add randowm spawn with increasing difficulty to factory. PROBLEM: projectiles do not kill enemies

--- src/sample/EnemyShip.java
+++ src/sample/EnemyShip.java
@@ -2,12 +2,4 @@ package sample;
 
 public class EnemyShip extends Ship {
 
-    private boolean isFriendly = false;
-    private double shotVelocity = -100;
-
-    public Projectile shoot() {
-        Projectile shot = new Projectile("sample/laserBlue06.png", 10, this.getPosition_x(), this.getPosition_y() + 0.5*this.getHeight() ,this.getShotVelocity());
-        shot.setFriendly(false);
-        return shot;
-    }
 }
